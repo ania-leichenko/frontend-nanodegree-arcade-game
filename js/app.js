c5c03fabb6c99dbbfc8427c0b64f3f916e5b7ded
Anna Leichenko <ania.leichenko@gmail.com>
2021-01-24 12:10:53 +0200

    refactoring and add winner text

--- js/app.js
+++ js/app.js
@@ -1,25 +1,19 @@
+window.ctx.font = "22px Verdana";
+window.ctx.strokeStyle = "white";
 const cellY = 83;
 const cellX = 100;
 const maxY = cellY * 5 - 10;
 const maxX = cellX * 4;
 
-var Enemy = function(x, y, speed, player) {
+const Enemy = function(x, y, speed) {
     this.x = x;
     this.y = y;
     this.speed = speed;
-    this.player = player;
     this.sprite = 'images/enemy-bug.png';
 };
 
 Enemy.prototype.update = function(dt) {
     this.x += this.speed;
-
-    if (this.x <= this.player.x + 80 && this.x >= this.player.x - 80) {
-      if(this.y < this.player.y && this.y > this.player.y - cellY) {
-        this.player.x = 200;
-        this.player.y = maxY;
-      }
-    }
 
     if (this.x > maxX + 100) {
       this.x = -100;
@@ -34,15 +28,20 @@ const Player = function(x, y) {
     this.x = x;
     this.y = y;
     this.sprite = 'images/char-boy.png';
+    
+    document.addEventListener('keyup', (event) => {
+      var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+      };
+      this.handleInput(allowedKeys[event.keyCode]);
+    });
 };
 
 Player.prototype.update = function() {
-  if(this.y === -10) {
-    setTimeout(()=> {
-      this.x = 200;
-      this.y = maxY;
-    }, 1000);
-  }
+  
 };
 
 Player.prototype.render = function() {
@@ -74,23 +73,77 @@ Player.prototype.handleInput = function(key) {
         this.y = maxY;
       } 
   }
-  console.log(this);
 }
 
-const player = new Player(200, maxY);
+const Text = function(text) {
+  this.text = text;
+};
 
-const allEnemies = [
-  new Enemy(10, 63, 1, player),
-  new Enemy(20, 150, 2, player),
-  new Enemy(40, 230, 3, player),
-];
- 
-document.addEventListener('keyup', function(event) {
-  var allowedKeys = {
-    37: 'left',
-    38: 'up',
-    39: 'right',
-    40: 'down'
-  };
-  player.handleInput(allowedKeys[event.keyCode]);
-});
+Text.prototype.update =function() {
+
+}
+
+Text.prototype.render = function() {
+  window.ctx.strokeText(this.text, 210, 100);
+}
+
+// --------------------------------------------------------
+const Controller = function(Player, Enemy, Text) {
+  this.gameResult = new Text('');
+
+  this.player = new Player(200, maxY);
+
+  this.enemies = [
+    new Enemy(10, 63, 1),
+    new Enemy(20, 150, 2),
+    new Enemy(40, 230, 3),
+  ];
+
+  this.items = [
+    ...this.enemies,
+    this.player,
+    this.gameResult,
+  ];
+};
+
+Controller.prototype.update = function() {
+  this.checkIfWin();
+  this.checkIfLose();
+
+  this.items.forEach(function(item) {
+    item.update();
+  });
+}
+
+
+Controller.prototype.checkIfWin = function() {
+  if(this.player.y === -10) {
+    this.gameResult.text = 'Winner';
+    setTimeout(()=> {
+      this.player.x = 200;
+      this.player.y = maxY;
+      this.gameResult.text = '';
+    }, 1000);
+  }
+}
+
+Controller.prototype.checkIfLose = function() {
+  this.enemies.forEach((enemy) => {
+    if (enemy.x <= this.player.x + 80 && enemy.x >= this.player.x - 80) {
+      if(enemy.y < this.player.y && enemy.y > this.player.y - cellY) {
+        this.player.x = 200;
+        this.player.y = maxY;
+      }
+    }
+  });
+}
+
+Controller.prototype.render = function() {
+  this.items.forEach(function(item) {
+      item.render();
+  });
+}
+// --------------------------------------------------------
+
+const controller = new Controller(Player, Enemy, Text);
+
